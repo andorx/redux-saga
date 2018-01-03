8f1d55ad2bda715a0b1dd90cd15032ef0ccc8e6b
Duc N. PHAM <pngduc@gmail.com>
2018-01-03 18:05:14 +0700

    Apply redux persist to store the state to localstorage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddlware from "redux-saga";
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import createSagaMiddlware from 'redux-saga';
+import {
+  persistStore,
+  persistReducer,
+  persistCombineReducers
+} from 'redux-persist';
+import storage from 'redux-persist/es/storage';
 
-import rootSaga from "./sagas";
-import reducer from "./reducers";
-import { Creators } from "./actions";
+import rootSaga from './sagas';
+import reducers from './reducers';
+import { Creators } from './actions';
 
 const sagaMiddleware = createSagaMiddlware();
+
+const persistConfig = {
+  key: 'ticker',
+  // debug: true,
+  storage
+};
+
+const reducer = persistReducer(persistConfig, reducers);
 
 const store = createStore(
   reducer,
@@ -15,13 +29,18 @@ const store = createStore(
   applyMiddleware(sagaMiddleware)
 );
 
+const persistor = persistStore(store);
+
 sagaMiddleware.run(rootSaga);
 
-store.dispatch(Creators.tickerRequest("bitcoin"));
-store.dispatch(Creators.tickerRequest("bitcoin"));
 // Invalid action with incorrect tickerId
 // store.dispatch(Creators.tickerRequest('badcoin'));
 
 store.subscribe(() => {
   console.log(store.getState());
 });
+
+// Prevent race codition execution
+setTimeout(() => {
+  store.dispatch(Creators.tickerRequest('ripple'));
+}, 5000);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { REHYDRATE, PURGE } from 'redux-persist';
 import { createReducer } from 'reduxsauce';
 
 import { Types } from '../actions';
@@ -5,33 +6,47 @@ import { Types } from '../actions';
 const INITIAL_STATE = {
   tickerId: undefined,
   ticker: {},
-  error: '',
+  error: ''
 };
 
 export const requestTicker = (state, action) => {
+  console.log('action', action);
   return {
     ...state,
     ticker: {},
-    tickerId: action.tickerId,
+    tickerId: action.tickerId
   };
 };
 
 export const tickerFetchingSuccess = (state, action) => {
   return {
     ...state,
-    ticker: action.ticker,
+    ticker: action.ticker
   };
 };
 
 export const tickerFetchingFailure = (state, action) => {
   return {
     ...state,
-    error: action.error,
+    error: action.error
   };
-}
+};
+
+export const rehydrate = (state, action) => {
+  return {
+    ...state,
+    ...action.payload
+  };
+};
+
+export const purge = (state, action) => {
+  return INITIAL_STATE;
+};
 
 export default createReducer(INITIAL_STATE, {
   [Types.TICKER_REQUEST]: requestTicker,
   [Types.TICKER_FETCHING_SUCCESS]: tickerFetchingSuccess,
   [Types.TICKER_FETCHING_FAILURE]: tickerFetchingFailure,
+  [REHYDRATE]: rehydrate,
+  [PURGE]: purge
 });
